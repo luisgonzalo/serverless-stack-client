5e6e72a4e61a6c237778699a482c01f04a2a417e
lgonzalo <lgonzalo@gmail.com>
2021-02-19 13:31:14 +0100

    Added notes list and new note and signin/signup

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import NewNote from "./containers/NewNote";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
@@ -8,6 +11,15 @@ export default function Routes() {
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/notes/new">
+        <NewNote />
       </Route>
       {/* Finally, catch all unmatched routes */}
       <Route>
