54a3c3b606b301d5d978e33f7798e287cdf97f44
lgonzalo <lgonzalo@gmail.com>
2021-02-19 12:11:21 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  s3: {
+    REGION: "eu-west-3",
+    BUCKET: "notes-uploaded-files",
+  },
+  apiGateway: {
+    REGION: "eu-west-3",
+    URL: "https://jxfubrgmp2.execute-api.eu-west-3.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "eu-west-3",
+    USER_POOL_ID: "eu-west-3_7BD3iblZ0",
+    APP_CLIENT_ID: "176holfddnl2t73949pqm7hrbt",
+    IDENTITY_POOL_ID: "eu-west-3:e08f3907-a239-473a-b6bb-70ac49fc0b3a",
+  },
+};
+
+export default config;
